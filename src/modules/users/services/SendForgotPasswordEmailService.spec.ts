8b186fccef203f964b2a874d547d06838329ba4e
Wilson Franca <wilson.franca.92@gmail.com>
2020-11-23 19:35:38 -0800

    test(resetpasswordservice): test sending the most simple email

--- src/modules/users/services/SendForgotPasswordEmailService.spec.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.spec.ts
@@ -1,17 +1,30 @@
 import AppError from '@shared/errors/AppError';
+import FakeEmailProvider from '@shared/container/providers/EmailProvider/fakes/FakeEmailProvider';
 import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
 import SendForgotPasswordEmailService from './SendForgotPasswordEmailService';
 
 describe('SendForgotPasswordEmailService', () => {
   it('should be able to recover the password using the email', async () => {
     const fakeUsersRepository = new FakeUsersRepository();
-    const sendEmail = new SendForgotPasswordEmailService(fakeUsersRepository);
-    await sendEmail.execute({
+    const fakeEmailProvider = new FakeEmailProvider();
+
+    const sendEmail = jest.spyOn(fakeEmailProvider, 'sendEmail');
+
+    const SendForgotPasswordEmail = new SendForgotPasswordEmailService(
+      fakeUsersRepository,
+      fakeEmailProvider,
+    );
+
+    await fakeUsersRepository.create({
       name: 'John Doe',
       email: 'johndoe@example.com',
-      password: '123456',
+      password: '1234',
     });
 
-    expect(user).toHaveProperty('id');
+    await SendForgotPasswordEmail.execute({
+      email: 'johndoe@example.com',
+    });
+
+    expect(sendEmail).toHaveBeenCalled();
   });
 });
