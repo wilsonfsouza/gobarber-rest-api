563a32534d7bedf73a6f149147736418bb829a3f
Wilson Franca <wilson.franca.92@gmail.com>
2020-11-23 18:50:05 -0800

    refactor(extensions): extracted logic of extensions and services shared by multiple modules

---
+++ src/shared/container/index.ts
@@ -0,0 +1,18 @@
+import { container } from 'tsyringe';
+import '@modules/users/providers';
+import './providers';
+import iAppointmentsRepository from '@modules/appointments/repositories/iAppointmentsRepository';
+import AppointmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository';
+
+import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
+
+container.registerSingleton<iAppointmentsRepository>(
+  'AppointmentsRepository',
+  AppointmentsRepository,
+);
+
+container.registerSingleton<IUsersRepository>(
+  'UsersRepository',
+  UsersRepository,
+);
