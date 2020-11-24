5d6295eac833dc9b769d7121946fadc03d36ca02
Wilson Franca <wilson.franca.92@gmail.com>
2020-11-23 19:37:37 -0800

    feat(sendemail): starter service to send email to users that are trying to reset a password

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -1,6 +1,7 @@
 // import AppError from '@shared/errors/AppError';
 // import User from '@modules/users/infra/typeorm/entities/User';
 import { inject, injectable } from 'tsyringe';
+import IEmailProvider from '@shared/container/providers/EmailProvider/models/IEmailProvider';
 import IUsersRepository from '../repositories/IUsersRepository';
 
 interface IRequest {
@@ -11,9 +12,17 @@ class SendForgotPasswordEmailService {
   constructor(
     @inject('UsersRepository')
     private usersRepository: IUsersRepository,
+
+    @inject('EmailProvider')
+    private emailProvider: IEmailProvider,
   ) { }
 
-  public async execute(): Promise<void> { }
+  public async execute({ email }: IRequest): Promise<void> {
+    this.emailProvider.sendEmail(
+      email,
+      'Your request to reset password was received.',
+    );
+  }
 }
 
 export default SendForgotPasswordEmailService;
