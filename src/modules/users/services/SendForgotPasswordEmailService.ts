add201906c9fbf934826c959b930fc2cb540c72e
Wilson Franca <wilson.franca.92@gmail.com>
2020-11-28 15:47:32 -0800

    feat(users): added SendForgotPasswordEmailService

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -1,8 +1,9 @@
-// import AppError from '@shared/errors/AppError';
-// import User from '@modules/users/infra/typeorm/entities/User';
 import { inject, injectable } from 'tsyringe';
+import path from 'path';
+import AppError from '@shared/errors/AppError';
 import IEmailProvider from '@shared/container/providers/EmailProvider/models/IEmailProvider';
 import IUsersRepository from '../repositories/IUsersRepository';
+import IUserTokensRepository from '../repositories/IUserTokensRepository';
 
 interface IRequest {
   email: string;
@@ -15,13 +16,40 @@ class SendForgotPasswordEmailService {
 
     @inject('EmailProvider')
     private emailProvider: IEmailProvider,
+
+    @inject('UserTokensRepository')
+    private userTokensRepository: IUserTokensRepository,
   ) { }
 
   public async execute({ email }: IRequest): Promise<void> {
-    this.emailProvider.sendEmail(
-      email,
-      'Your request to reset password was received.',
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new AppError('This user does not exist.');
+    }
+
+    const { token } = await this.userTokensRepository.generate(user.id);
+
+    const forgotPasswordTemplate = path.resolve(
+      __dirname,
+      '..',
+      'views',
+      'forgot_password.hbs',
     );
+    await this.emailProvider.sendEmail({
+      to: {
+        name: user.name,
+        email: user.email,
+      },
+      subject: '[GoBarber] Password Recovery',
+      templateData: {
+        file: forgotPasswordTemplate,
+        variables: {
+          name: user.name,
+          link: `http://localhost:3000/reset_password?token=${token}`,
+        },
+      },
+    });
   }
 }
 
