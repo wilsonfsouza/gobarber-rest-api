40308c8cdecba3d7a2f7910beb41150a5dd5218d
Wilson Franca <wilson.franca.92@gmail.com>
2020-12-01 20:59:44 -0800

    refactor(appointments): clean up unnecessary comments

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -6,12 +6,6 @@ const appointmentsRouter = Router();
 const appointmentsController = new AppointmentsController();
 appointmentsRouter.use(ensureAuthenticated);
 
-// appointmentsRouter.get('/', async (request, response) => {
-//   const appointments = await appointmentsRepository.find();
-
-//   return response.json(appointments);
-// });
-
 appointmentsRouter.post('/', appointmentsController.create);
 
 export default appointmentsRouter;
