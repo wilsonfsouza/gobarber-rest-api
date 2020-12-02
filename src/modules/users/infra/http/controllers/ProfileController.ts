d87fc5f88138a9f8b92ff21e33d0c6d28d4bb292
Wilson Franca <wilson.franca.92@gmail.com>
2020-12-01 19:51:41 -0800

    feat(users): profile update controller

---
+++ src/modules/users/infra/http/controllers/ProfileController.ts
@@ -0,0 +1,32 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+import UpdateProfileService from '@modules/users/services/UpdateProfileService';
+import ShowProfileService from '@modules/users/services/ShowProfileService';
+
+export default class ProfileController {
+  public async show(request: Request, response: Response): Promise<Response> {
+    const user_id = request.user.id;
+    const userProfile = container.resolve(ShowProfileService);
+    const userProfileInformation = await userProfile.execute({ user_id });
+    delete userProfileInformation.password;
+    return response.json(userProfileInformation);
+  }
+
+  public async update(request: Request, response: Response): Promise<Response> {
+    const user_id = request.user.id;
+    const { name, email, old_password, password } = request.body;
+    const updateProfile = container.resolve(UpdateProfileService);
+
+    const user = await updateProfile.execute({
+      user_id,
+      name,
+      email,
+      old_password,
+      password,
+    });
+
+    delete user.password;
+
+    return response.json(user);
+  }
+}
