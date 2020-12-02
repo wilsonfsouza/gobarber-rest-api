36f7f9c60efe3acff21f3e7680707a0084b7f39d
Wilson Franca <wilson.franca.92@gmail.com>
2020-12-01 19:43:28 -0800

    refactor(shared): container for depedency injection

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -7,6 +7,9 @@ import AppointmentsRepository from '@modules/appointments/infra/typeorm/reposito
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
 
+import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository';
+import UserTokensRepository from '@modules/users/infra/typeorm/repositories/UserTokensRepository';
+
 container.registerSingleton<iAppointmentsRepository>(
   'AppointmentsRepository',
   AppointmentsRepository,
@@ -15,4 +18,9 @@ container.registerSingleton<iAppointmentsRepository>(
 container.registerSingleton<IUsersRepository>(
   'UsersRepository',
   UsersRepository,
+);
+
+container.registerSingleton<IUserTokensRepository>(
+  'UserTokensRepository',
+  UserTokensRepository,
 );
