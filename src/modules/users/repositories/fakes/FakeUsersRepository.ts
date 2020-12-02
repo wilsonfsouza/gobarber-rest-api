0c8d7ca784fb40c9c20f55510d5d2f3b26fcf25f
Wilson Franca <wilson.franca.92@gmail.com>
2020-12-01 20:19:29 -0800

    feat(users): added find all providers method to fake users repository

--- src/modules/users/repositories/fakes/FakeUsersRepository.ts
+++ src/modules/users/repositories/fakes/FakeUsersRepository.ts
@@ -6,6 +6,16 @@ import User from '../../infra/typeorm/entities/User';
 class FakeUsersRepository implements IUsersRepository {
   private users: User[] = [];
 
+  public async findAllProviders(except_user_id?: string): Promise<User[]> {
+    let { users } = this;
+
+    if (except_user_id) {
+      users = this.users.filter(user => user.id !== except_user_id);
+    }
+
+    return users;
+  }
+
   public async findById(id: string): Promise<User | undefined> {
     const findUser = this.users.find(user => user.id === id);
     return findUser;
