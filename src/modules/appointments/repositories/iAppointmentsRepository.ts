48ab671585e256c299dc47c9c66653c968a3d444
Wilson Franca <wilson.franca.92@gmail.com>
2020-12-03 20:31:14 -0800

    feat(appointments): added find all appointments by month for a given provider method definition for interface

--- src/modules/appointments/repositories/iAppointmentsRepository.ts
+++ src/modules/appointments/repositories/iAppointmentsRepository.ts
@@ -1,7 +1,11 @@
 import Appointment from '../infra/typeorm/entities/Appointment';
 import ICreateAppointmentsDTO from '../dtos/ICreateAppointmentsDTO';
+import IFindAllByMonthForOneProviderDTO from '../dtos/IFindAllByMonthForOneProviderDTO';
 
 export default interface IAppointmentsRepository {
   create(data: ICreateAppointmentsDTO): Promise<Appointment>;
   findByDate(date: Date): Promise<Appointment | undefined>;
+  findAllByMonthForOneProvider(
+    data: IFindAllByMonthForOneProviderDTO,
+  ): Promise<Appointment[]>;
 }
