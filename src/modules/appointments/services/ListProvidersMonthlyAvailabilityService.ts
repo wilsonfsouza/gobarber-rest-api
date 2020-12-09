99805323bee434be64db17c781dd5770a51b4cb6
Wilson Franca <wilson.franca.92@gmail.com>
2020-12-08 19:30:03 -0800

    feat(appointments): added listing of date availability for appointments

---
+++ src/modules/appointments/services/ListProvidersMonthlyAvailabilityService.ts
@@ -0,0 +1,62 @@
+import { inject, injectable } from 'tsyringe';
+import { getDaysInMonth, getDate } from 'date-fns';
+// import User from '@modules/users/infra/typeorm/entities/User';
+// import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import IAppointmentsRepository from '@modules/appointments/repositories/iAppointmentsRepository';
+
+interface IRequest {
+  provider_id: string;
+  month: number;
+  year: number;
+}
+
+type IResponse = Array<{
+  day: number;
+  available: boolean;
+}>;
+
+@injectable()
+class ListProvidersMonthlyAvailabilityService {
+  constructor(
+    @inject('AppointmentsRespository')
+    private appointmentsRepository: IAppointmentsRepository,
+  ) { }
+
+  public async execute({
+    provider_id,
+    month,
+    year,
+  }: IRequest): Promise<IResponse> {
+    const appointments = await this.appointmentsRepository.findAllByMonthForOneProvider(
+      {
+        provider_id,
+        year,
+        month,
+      },
+    );
+
+    const numberOfDaysInMonth = getDaysInMonth(new Date(year, month - 1));
+
+    const eachDayArray = Array.from(
+      {
+        length: numberOfDaysInMonth,
+      },
+      (_, index) => index + 1,
+    );
+
+    const availability = eachDayArray.map(day => {
+      const appointmentsInDay = appointments.filter(appointment => {
+        return getDate(appointment.date) === day;
+      });
+
+      return {
+        day,
+        available: appointmentsInDay.length < 10,
+      };
+    });
+
+    return availability;
+  }
+}
+
+export default ListProvidersMonthlyAvailabilityService;
