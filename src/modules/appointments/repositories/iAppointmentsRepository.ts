523e2d242f220f7a01504d1286a2783c3a37d723
Wilson Franca <wilson.franca.92@gmail.com>
2020-12-08 19:59:03 -0800

    feat(appointments): added find all appointments by day to the interface

--- src/modules/appointments/repositories/iAppointmentsRepository.ts
+++ src/modules/appointments/repositories/iAppointmentsRepository.ts
@@ -1,11 +1,15 @@
 import Appointment from '../infra/typeorm/entities/Appointment';
 import ICreateAppointmentsDTO from '../dtos/ICreateAppointmentsDTO';
 import IFindAllByMonthForOneProviderDTO from '../dtos/IFindAllByMonthForOneProviderDTO';
+import IFindAllByDayForOneProviderDTO from '../dtos/IFindAllByDayForOneProviderDTO';
 
 export default interface IAppointmentsRepository {
   create(data: ICreateAppointmentsDTO): Promise<Appointment>;
   findByDate(date: Date): Promise<Appointment | undefined>;
   findAllByMonthForOneProvider(
     data: IFindAllByMonthForOneProviderDTO,
+  ): Promise<Appointment[]>;
+  findAllByDayForOneProvider(
+    data: IFindAllByDayForOneProviderDTO,
   ): Promise<Appointment[]>;
 }
