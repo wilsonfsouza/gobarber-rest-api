87e157742db3a9d9e08db90fa6a1992fdf20f53d
Wilson Franca <wilson.franca.92@gmail.com>
2020-12-10 20:54:15 -0800

    feat(appointments): added routes for daily and monthly availability for providers

--- src/modules/appointments/infra/http/routes/providers.routes.ts
+++ src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -1,11 +1,23 @@
 import { Router } from 'express';
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import ProvidersController from '../controllers/ProvidersController';
+import ProviderDailyAvailabilityController from '../controllers/ProviderDailyAvailabilityController';
+import ProviderMonthlyAvailabilityController from '../controllers/ProviderMonthlyAvailabilityController';
 
 const providersRouter = Router();
 const providersController = new ProvidersController();
+const providerDailyAvailabilityController = new ProviderDailyAvailabilityController();
+const providerMonthlyAvailabilityController = new ProviderMonthlyAvailabilityController();
 providersRouter.use(ensureAuthenticated);
 
 providersRouter.get('/', providersController.index);
+providersRouter.get(
+  '/:provider_id/monthly-availability',
+  providerMonthlyAvailabilityController.index,
+);
+providersRouter.get(
+  '/:provider_id/daily-availability',
+  providerDailyAvailabilityController.index,
+);
 
 export default providersRouter;
