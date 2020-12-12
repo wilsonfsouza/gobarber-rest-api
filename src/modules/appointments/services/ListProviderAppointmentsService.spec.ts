e20d0b9f7d87181f2d6abd1260538f088443bed0
Wilson Franca <wilson.franca.92@gmail.com>
2020-12-11 20:07:44 -0800

    tests(appointments): created a unit tests to list all appointments a provider has on a given date

---
+++ src/modules/appointments/services/ListProviderAppointmentsService.spec.ts
@@ -0,0 +1,40 @@
+import AppError from '@shared/errors/AppError';
+
+import FakeAppointmentsRepository from '@modules/appointments/repositories/fakes/FakeAppointmentsRepository';
+import ListProviderAppointmentsService from './ListProviderAppointmentsService';
+
+let fakeAppointmentsRepository: FakeAppointmentsRepository;
+let listProviderAppointmentsService: ListProviderAppointmentsService;
+
+describe('ListProvidersMonthlyAvailability', () => {
+  beforeEach(() => {
+    fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    listProviderAppointmentsService = new ListProviderAppointmentsService(
+      fakeAppointmentsRepository,
+    );
+  });
+  it('should be able to show all appointments that a provider has on a given date', async () => {
+    const appointment1 = await fakeAppointmentsRepository.create({
+      provider_id: 'provider',
+      user_id: 'another-user',
+      date: new Date(2020, 4, 20, 14, 0, 0),
+    });
+
+    const appointment2 = await fakeAppointmentsRepository.create({
+      provider_id: 'provider',
+      user_id: 'another-user',
+      date: new Date(2020, 4, 20, 16, 0, 0),
+    });
+
+    const appointments = await listProviderAppointmentsService.execute({
+      provider_id: 'provider',
+      day: 20,
+      month: 5,
+      year: 2020,
+    });
+
+    expect(appointments).toEqual(
+      expect.arrayContaining([appointment1, appointment2]),
+    );
+  });
+});
