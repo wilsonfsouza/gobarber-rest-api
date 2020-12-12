ae3a9b5a52e804a5028749a9a869be73f7e83758
Wilson Franca <wilson.franca.92@gmail.com>
2020-12-11 20:07:12 -0800

    feat(appointments): created a service to list all appointments a provider has on a given date

---
+++ src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -0,0 +1,37 @@
+import { inject, injectable } from 'tsyringe';
+import IAppointmentsRepository from '@modules/appointments/repositories/iAppointmentsRepository';
+import Appointment from '../infra/typeorm/entities/Appointment';
+
+interface IRequest {
+  provider_id: string;
+  day: number;
+  month: number;
+  year: number;
+}
+
+@injectable()
+class ListProviderAppointmentsService {
+  constructor(
+    @inject('AppointmentsRepository')
+    private appointmentsRepository: IAppointmentsRepository,
+  ) { }
+
+  public async execute({
+    provider_id,
+    day,
+    month,
+    year,
+  }: IRequest): Promise<Appointment[]> {
+    const appointments = await this.appointmentsRepository.findAllByDayForOneProvider(
+      {
+        provider_id,
+        day,
+        month,
+        year,
+      },
+    );
+    return appointments;
+  }
+}
+
+export default ListProviderAppointmentsService;
