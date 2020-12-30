51e3d14979decd8d549884785b88c224a4b34b99
Wilson Franca <wilson.franca.92@gmail.com>
2020-12-30 11:03:41 -0800

    refactor(shared container): imported isolated container for each provider, using this file to organize imports.

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -1,24 +1,3 @@
-import { container } from 'tsyringe';
-import IStorageProvider from './StorageProvider/models/IStorageProvider';
-import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider';
-
-import EtherealEmailProvider from './EmailProvider/implementations/EtherealEmailProvider';
-import IEmailProvider from './EmailProvider/models/IEmailProvider';
-
-import IEmailTemplateProvider from './EmailTemplateProvider/models/IEmailTemplateProvider';
-import HandlebarsEmailTemplateProvider from './EmailTemplateProvider/implementations/HandlebarsEmailTemplateProvider';
-
-container.registerSingleton<IStorageProvider>(
-  'StorageProvider',
-  DiskStorageProvider,
-);
-
-container.registerSingleton<IEmailTemplateProvider>(
-  'EmailTemplateProvider',
-  HandlebarsEmailTemplateProvider,
-);
-
-container.registerInstance<IEmailProvider>(
-  'EmailProvider',
-  container.resolve(EtherealEmailProvider),
-);
+import './StorageProvider';
+import './EmailTemplateProvider';
+import './EmailProvider';
