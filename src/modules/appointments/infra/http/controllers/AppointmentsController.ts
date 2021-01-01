14bfee43ba60f7fb841c8d6df0d263a595e84621
Wilson Franca <wilson.franca.92@gmail.com>
2020-12-31 16:49:00 -0800

    refactor(appointments): after adding field validation to date with Joi, the field is already converted. Thus, we do not need to parseISO anymore.

--- src/modules/appointments/infra/http/controllers/AppointmentsController.ts
+++ src/modules/appointments/infra/http/controllers/AppointmentsController.ts
@@ -1,5 +1,4 @@
 import { Request, Response } from 'express';
-import { parseISO } from 'date-fns';
 import { container } from 'tsyringe';
 import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
 
@@ -8,13 +7,12 @@ export default class AppointmentsController {
     const { provider_id, date } = request.body;
     const user_id = request.user.id;
 
-    const parsedDate = parseISO(date);
     const createAppointment = container.resolve(CreateAppointmentService);
 
     const appointment = await createAppointment.execute({
       provider_id,
       user_id,
-      date: parsedDate,
+      date,
     });
 
     return response.json(appointment);
