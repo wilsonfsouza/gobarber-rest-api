3b63d08e9c21baaaf1fa931dbd132a42fb4b9156
Wilson Franca <wilson.franca.92@gmail.com>
2020-12-31 16:46:36 -0800

    feat(cacheProvider): created redis implementation for caching expensive queries of the application with methods to save, invalidate, invalidatePrefix, and retrive. Used ioredis because it is fast and supports async-await calls. The pipeline method was used to delete multiple keys with the same prefix at the same time without blocking the execution of other operations. Since the app has less than 1M users, this method was a great start. Once the app reaches more than that, we could considering using the method streaming instead for better performance.

---
+++ src/shared/container/providers/CacheProvider/implementations/RedisCacheProvider.ts
@@ -0,0 +1,44 @@
+import Redis, { Redis as RedisClientType } from 'ioredis';
+import cacheConfig from '@config/cache';
+import { AnyLengthString } from 'aws-sdk/clients/comprehendmedical';
+import ICacheProvider from '../models/ICacheProvider';
+
+export default class RedisCacheProvider implements ICacheProvider {
+  private client: RedisClientType;
+
+  constructor() {
+    this.client = new Redis(cacheConfig.config.redis);
+  }
+
+  public async save(key: string, value: any): Promise<void> {
+    await this.client.set(key, JSON.stringify(value));
+  }
+
+  public async retrieve<T>(key: string): Promise<T | null> {
+    const data = await this.client.get(key);
+
+    if (!data) {
+      return null;
+    }
+
+    const parsedData = JSON.parse(data) as T;
+
+    return parsedData;
+  }
+
+  public async invalidate(key: string): Promise<void> {
+    await this.client.del(key);
+  }
+
+  public async invalidatePrefix(prefix: string): Promise<void> {
+    const keys = await this.client.keys(`${prefix}:*`);
+
+    const pipeline = this.client.pipeline();
+
+    keys.forEach(key => {
+      pipeline.del(key);
+    });
+
+    await pipeline.exec();
+  }
+}
