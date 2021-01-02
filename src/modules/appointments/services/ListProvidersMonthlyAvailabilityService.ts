22bd315cad748b0edff2cd13bc53f7c37940ab23
Wilson Franca <wilson.franca.92@gmail.com>
2021-01-01 17:34:32 -0800

    feat(appointments): invalidated past dates, so the user can not search for provider availability in a past month.

--- src/modules/appointments/services/ListProvidersMonthlyAvailabilityService.ts
+++ src/modules/appointments/services/ListProvidersMonthlyAvailabilityService.ts
@@ -1,7 +1,6 @@
 import { inject, injectable } from 'tsyringe';
-import { getDaysInMonth, getDate } from 'date-fns';
-// import User from '@modules/users/infra/typeorm/entities/User';
-// import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import { getDaysInMonth, getDate, isAfter } from 'date-fns';
+
 import IAppointmentsRepository from '@modules/appointments/repositories/iAppointmentsRepository';
 
 interface IRequest {
@@ -45,13 +44,16 @@ class ListProvidersMonthlyAvailabilityService {
     );
 
     const availability = eachDayArray.map(day => {
+      const compareDate = new Date(year, month - 1, day, 23, 59, 59);
+
       const appointmentsInDay = appointments.filter(appointment => {
         return getDate(appointment.date) === day;
       });
 
       return {
         day,
-        available: appointmentsInDay.length < 10,
+        available:
+          isAfter(compareDate, new Date()) && appointmentsInDay.length < 10,
       };
     });
 
